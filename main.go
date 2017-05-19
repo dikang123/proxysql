5c1a015eb1fccdf64af6e81846de8ed53d1e8474
Tian, Lei <tianlei@paratera.com>
2017-05-19 15:46:40 +0800

    add api files

--- main.go
+++ main.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	//	"context"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	//	"github.com/pressly/chi"
-	//	"github.com/pressly/chi/middleware"
 	"log"
-	"proxysql-master/admin/cmd"
-	"proxysql-master/admin/servers"
+	//"proxysql-master/admin/cmd"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"net/http"
+	//	"proxysql-master/admin/servers"
 	"proxysql-master/admin/users"
 )
 
+var (
+	db  *sql.DB
+	err error
+)
+
 func main() {
-	db, err := sql.Open("mysql", "admin:admin@tcp(172.18.7.204:6032)/main?charset=utf8")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	db, err = sql.Open("mysql", "admin:admin@tcp(172.18.7.204:6032)/main?charset=utf8")
 	if err != nil {
 		log.Fatal("Open()", err)
 	}
 	defer db.Close()
 
-	var proxysql_users users.Users
-	proxysql_users.Username = "tianlei2"
-	proxysql_users.Password = "111111"
+	e.GET("/users", listAllUsers)
+	e.POST("/users", createUser)
+	e.GET("/users/:username", listOneUser)
+	//	e.PUT("/users/:username", updateUsers)
+	e.DELETE("/users/:username", deleteOneUser)
 
-	var proxysql_servers servers.Servers
-	proxysql_servers.HostGroupId = 3
-	proxysql_servers.HostName = "dn10"
-	proxysql_servers.Port = 3310
-
-	proxysql_users.AddOneUser(db)
-	//proxysql_users.FindOneUserInfo(db)
-	//users.FindAllUserInfo(db)
-	//proxysql_users.DeleteOneUser(db)
-
-	proxysql_servers.AddOneServers(db)
-	//proxysql_servers.FindOneServersInfo(db)
-	proxysql_servers.DeleteOneServers(db)
-	//servers.FindAllServerInfo(db)
-	cmd.LoadUserToRuntime(db)
-	cmd.SaveUserToDisk(db)
+	e.Logger.Fatal(e.Start(":3333"))
 }

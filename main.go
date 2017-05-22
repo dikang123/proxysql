62ecf65e41dfef54651084668cd454f853bd83b4
Tian, Lei <tianlei@paratera.com>
2017-05-22 09:37:45 +0800

    调整代码结构

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 	//	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+	//_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 	//	"github.com/labstack/echo/middleware"
-	"log"
+	//"log"
 	//	"net/http"
 	"proxysql-master/pmapi"
 	//	"proxysql-master/admin/servers"
@@ -15,11 +15,11 @@ import (
 )
 
 func main() {
-	var err error
+	//var err error
 	pmapiv1 := new(pmapi.PMApi)
 
 	pmapiv1.Echo = echo.New()
-	e := pmapiv1.Echo
+	//e := pmapiv1.Echo
 	pmapiv1.PMuser = "admin"
 	pmapiv1.PMpass = "admin"
 	pmapiv1.PMhost = "172.18.7.204:6032"
@@ -28,17 +28,10 @@ func main() {
 
 	pmapiv1.RegisterMiddleware()
 
-	pmapiv1.Apidb, err = sql.Open("mysql", pmapiv1.PMdbi)
-	if err != nil {
-		log.Fatal("Open()", err)
-	}
-	defer pmapiv1.Apidb.Close()
+	pmapiv1.RegisterDBInterface()
 
-	e.GET("/users", pmapiv1.ListAllUsers)
-	e.POST("/users", pmapiv1.CreateUser)
-	e.GET("/users/:username", pmapiv1.ListOneUser)
-	//	e.PUT("/users/:username", updateUsers)
-	e.DELETE("/users/:username", pmapiv1.DeleteOneUser)
+	pmapiv1.RegisterServices()
 
-	e.Logger.Fatal(e.Start(":3333"))
+	pmapiv1.RunApiService()
+
 }

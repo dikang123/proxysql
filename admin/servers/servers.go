d34a73114b836befe8eb5544211446116ce32cae
Lei Tian <taylor840326@gmail.com>
2017-05-24 11:19:17 +0800

    把连接池的定义放在admin/status/status.go中

--- admin/servers/servers.go
+++ admin/servers/servers.go
@@ -21,21 +21,6 @@ type Servers struct {
 	Comment           string `db:"comment" json:"comment"`
 }
 
-type MySQLConnectionsPool struct {
-	HostGroup     uint64 `db:"hostgroup" json:"hostgroup"`
-	SrvHost       string `db:"srv_host" json:"srv_host"`
-	SrvPort       uint64 `db:"srv_port" json:"srv_port"`
-	Status        string `db:"status" json:"status"`
-	ConnUsed      uint64 `db:"ConnUsed" json:"ConnUsed"`
-	ConnFree      uint64 `db:"ConnFree" json:"ConnFree"`
-	ConnOK        uint64 `db:"ConnOK" json:"ConnOK"`
-	ConnERR       uint64 `db:"ConnERR" json:"ConnERR"`
-	Queries       uint64 `db:"Queries" json:"Queries"`
-	BytesDataSent uint64 `db:"Bytes_data_sent" json:"Bytes_data_sent"`
-	BytesDataRecv uint64 `db:"Bytes_data_recv" json:"Bytes_data_recv"`
-	LatencyUs     uint64 `db:"Latency_us" json:"Latency_us"`
-}
-
 const (
 	StmtAddOneServers          = `INSERT INTO mysql_servers(hostgroup_id,hostname,port) VALUES(%d,%q,%d)`
 	StmtDeleteOneServers       = `DELETE FROM mysql_servers WHERE hostgroup_id=%d AND hostname=%q AND port=%d`

3b82777dbb945b24e91c53ecd8dab20afcd31e6b
Tian, Lei <tianlei@paratera.com>
2017-06-08 11:00:10 +0800

    去掉所有的log.Fatal语句，并且添加UpdateOneUserPass函数可修改用户密码

--- admin/users/users.go
+++ admin/users/users.go
@@ -36,29 +36,30 @@ type (
 )
 
 const (
-	StmtUserExist        = `SELECT count(*) FROM mysql_users WHERE username = %q`
-	StmtAddOneUser       = `INSERT INTO mysql_users(username,password) VALUES(%q,%q)`
-	StmtDeleteOneUser    = `DELETE FROM mysql_users WHERE username = %q`
-	StmtActiveOneUser    = `UPDATE mysql_users SET active = 1 WHERE username = %q`
-	StmtDisactiveOneUser = `UPDATE mysql_users SET active = 0 WHERE username = %q`
-	StmtFindOneUserInfo  = `SELECT * FROM mysql_users WHERE username = %q`
-	StmtFindAllUserInfo  = `SELECT * FROM mysql_users limit %d offset %d`
-	StmtUpdateOneUserDs  = `UPDATE mysql_users SET default_schema=%q WHERE username = %q`
-	StmtUpdateOneUserMc  = `UPDATE mysql_users SET max_connections = %d WHERE username = %q`
-	StmtUpdateOneUserDH  = `UPDATE mysql_users SET default_hostgroup=%d WHERE username = %q`
+	StmtUserExist         = `SELECT count(*) FROM mysql_users WHERE username = %q`
+	StmtAddOneUser        = `INSERT INTO mysql_users(username,password) VALUES(%q,%q)`
+	StmtDeleteOneUser     = `DELETE FROM mysql_users WHERE username = %q`
+	StmtActiveOneUser     = `UPDATE mysql_users SET active = 1 WHERE username = %q`
+	StmtDisactiveOneUser  = `UPDATE mysql_users SET active = 0 WHERE username = %q`
+	StmtFindOneUserInfo   = `SELECT * FROM mysql_users WHERE username = %q`
+	StmtFindAllUserInfo   = `SELECT * FROM mysql_users limit %d offset %d`
+	StmtUpdateOneUserDs   = `UPDATE mysql_users SET default_schema=%q WHERE username = %q`
+	StmtUpdateOneUserMc   = `UPDATE mysql_users SET max_connections = %d WHERE username = %q`
+	StmtUpdateOneUserDH   = `UPDATE mysql_users SET default_hostgroup=%d WHERE username = %q`
+	StmtUpdateOneUserPass = `UPDATE mysql_users SET password=%q WHERE username = %q`
 )
 
 func (users *Users) UserExists(db *sql.DB) bool {
 	st := fmt.Sprintf(StmtUserExist, users.Username)
 	rows, err := db.Query(st)
 	if err != nil {
-		log.Fatal("UserExists:", err)
+		//log.Fatal("UserExists:", err)
 	}
 	var UserCount uint64
 	for rows.Next() {
 		err = rows.Scan(&UserCount)
 		if err != nil {
-			log.Fatal("UserExists,Scan:", err)
+			//log.Fatal("UserExists,Scan:", err)
 		}
 	}
 	if UserCount == 0 {
@@ -100,12 +101,12 @@ func (users *Users) ActiveOneUser(db *sql.DB) int {
 		st := fmt.Sprintf(StmtActiveOneUser, users.Username)
 		_, err := db.Query(st)
 		if err != nil {
-			log.Fatal("ActiveOneUser:", err)
+			//log.Fatal("ActiveOneUser:", err)
 			return 1
 		}
 		return 0
 	} else {
-		log.Fatal("ActiveOneUser: User is not exists")
+		//log.Fatal("ActiveOneUser: User is not exists")
 		return 2
 	}
 }
@@ -115,12 +116,12 @@ func (users *Users) DisactiveOneUser(db *sql.DB) int {
 		st := fmt.Sprintf(StmtDisactiveOneUser, users.Username)
 		_, err := db.Query(st)
 		if err != nil {
-			//log.Fatal("DisactiveOneUser:", err)
+			////log.Fatal("DisactiveOneUser:", err)
 			return 1
 		}
 		return 0
 	} else {
-		//log.Fatal("DisactiveOneUser: User is not exists")
+		////log.Fatal("DisactiveOneUser: User is not exists")
 		return 2
 	}
 }
@@ -130,12 +131,27 @@ func (users *Users) UpdateOneUserDh(db *sql.DB) int {
 		st := fmt.Sprintf(StmtUpdateOneUserDH, users.DefaultHostgroup, users.Username)
 		_, err := db.Query(st)
 		if err != nil {
-			log.Fatal("UpdateOneUserDH()", err)
+			//log.Fatal("UpdateOneUserDH()", err)
 			return 1
 		}
 		return 0
 	} else {
-		log.Fatal("UpdateOneUserDH()", "User is not exists")
+		//log.Fatal("UpdateOneUserDH()", "User is not exists")
+		return 2
+	}
+}
+
+func (users *Users) UpdateOneUserPass(db *sql.DB) int {
+	if isexist := users.UserExists(db); isexist == true {
+		st := fmt.Sprintf(StmtUpdateOneUserPass, users.Password, users.Username)
+		_, err := db.Query(st)
+		if err != nil {
+			//log.Fatal("UpdateOneUserPass()", err)
+			return 1
+		}
+		return 0
+	} else {
+		//log.Fatal("UpdateOneUserPass()", "User is not exists")
 		return 2
 	}
 }
@@ -145,12 +161,12 @@ func (users *Users) UpdateOneUserDs(db *sql.DB) int {
 		st := fmt.Sprintf(StmtUpdateOneUserDs, users.DefaultSchema, users.Username)
 		_, err := db.Query(st)
 		if err != nil {
-			log.Fatal("UpdateOneUserDs:", err)
+			//log.Fatal("UpdateOneUserDs:", err)
 			return 1
 		}
 		return 0
 	} else {
-		log.Fatal("UpdateOneUserDs: User is not exists")
+		//log.Fatal("UpdateOneUserDs: User is not exists")
 		return 2
 	}
 }
@@ -160,12 +176,12 @@ func (users *Users) UpdateOneUserMc(db *sql.DB) int {
 		st := fmt.Sprintf(StmtUpdateOneUserMc, users.MaxConnections, users.Username)
 		_, err := db.Query(st)
 		if err != nil {
-			log.Fatal("UpdateOneUserMc:", err)
+			//log.Fatal("UpdateOneUserMc:", err)
 			return 1
 		}
 		return 0
 	} else {
-		log.Fatal("UpdateOneUserMc: User is not exists")
+		//log.Fatal("UpdateOneUserMc: User is not exists")
 		return 2
 	}
 }
@@ -175,7 +191,7 @@ func (users *Users) FindOneUserInfo(db *sql.DB) Users {
 		st := fmt.Sprintf(StmtFindOneUserInfo, users.Username)
 		rows, err := db.Query(st)
 		if err != nil {
-			log.Fatal("UpdateOneUserMc:", err)
+			//log.Fatal("UpdateOneUserMc:", err)
 		}
 		for rows.Next() {
 			err = rows.Scan(&users.Username,
@@ -192,7 +208,7 @@ func (users *Users) FindOneUserInfo(db *sql.DB) Users {
 				&users.MaxConnections)
 		}
 	} else {
-		log.Fatal("UpdateOneUserMc: User is not exists")
+		//log.Fatal("UpdateOneUserMc: User is not exists")
 	}
 	return *users
 }
@@ -204,7 +220,7 @@ func FindAllUserInfo(db *sql.DB, limit int64, skip int64) []Users {
 	QueryText = fmt.Sprintf(StmtFindAllUserInfo, limit, skip)
 	rows, err := db.Query(QueryText)
 	if err != nil {
-		log.Fatal("FindAllUserInfo:", err)
+		//log.Fatal("FindAllUserInfo:", err)
 	}
 	defer rows.Close()
 	for rows.Next() {

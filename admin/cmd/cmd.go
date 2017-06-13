74818361dcd5b8771e3f7329e1e24abe8fad52ba
Lei Tian <taylor840326@gmail.com>
2017-06-13 15:28:08 +0800

    把admin/cmd/cmd.go文件中添加log.Print日志

--- admin/cmd/cmd.go
+++ admin/cmd/cmd.go
@@ -28,128 +28,200 @@ const (
 	CmdSaveSchedulerToDisk     = `SAVE SCHEDULER TO DISK`
 )
 
-func ProxyReadOnly(db *sql.DB) {
+func ProxyReadOnly(db *sql.DB) int {
+	log.Print("ProxyReadOnly: ", CmdProxyReadOnly)
 	_, err := db.Query(CmdProxyReadOnly)
 	if err != nil {
 		log.Print("ProxyReadOnly:", err)
+		return 1
 	}
+	log.Print("ProxyReadOnly: Success")
+	return 0
 }
 
-func ProxyReadWrite(db *sql.DB) {
+func ProxyReadWrite(db *sql.DB) int {
+	log.Print("ProxyReadWrite: ", CmdProxyReadWrite)
 	_, err := db.Query(CmdProxyReadWrite)
 	if err != nil {
 		log.Print("ProxyReadWrite:", err)
+		return 1
 	}
+	log.Print("ProxyReadWrite: Success")
+	return 0
 }
 
-func ProxyStart(db *sql.DB) {
+func ProxyStart(db *sql.DB) int {
+	log.Print("ProxyStart: ", CmdProxyStart)
 	_, err := db.Query(CmdProxyStart)
 	if err != nil {
 		log.Print("ProxyStart:", err)
+		return 1
 	}
+	log.Print("ProxyStart: Success")
+	return 0
 }
 
-func ProxyRestart(db *sql.DB) {
+func ProxyRestart(db *sql.DB) int {
+	log.Print("ProxyRestart: ", CmdProxyRestart)
 	_, err := db.Query(CmdProxyRestart)
 	if err != nil {
 		log.Print("ProxyRestart:", err)
+		return 1
 	}
+	log.Print("ProxyRestart: Success")
+	return 0
 }
 
-func ProxyStop(db *sql.DB) {
+func ProxyStop(db *sql.DB) int {
+	log.Print("ProxyStop: ", CmdProxyStop)
 	_, err := db.Query(CmdProxyStop)
 	if err != nil {
 		log.Print("ProxyStop:", err)
+		return 1
 	}
+	log.Print("ProxyStop: Success")
+	return 0
 }
 
-func ProxyPause(db *sql.DB) {
+func ProxyPause(db *sql.DB) int {
+	log.Print("ProxyPause: ", CmdProxyStop)
 	_, err := db.Query(CmdProxyPause)
 	if err != nil {
 		log.Print("ProxyPause:", err)
+		return 1
 	}
+	log.Print("ProxyPause: Success")
+	return 0
 }
 
-func ProxyResume(db *sql.DB) {
+func ProxyResume(db *sql.DB) int {
+	log.Print("ProxyResume: ", CmdProxyResume)
 	_, err := db.Query(CmdProxyResume)
 	if err != nil {
 		log.Print("ProxyResume:", err)
+		return 1
 	}
+	log.Print("ProxyResume: Success")
+	return 0
 }
 
-func ProxyShutdown(db *sql.DB) {
+func ProxyShutdown(db *sql.DB) int {
+	log.Print("ProxyShutdown: ", CmdProxyShutdown)
 	_, err := db.Query(CmdProxyShutdown)
 	if err != nil {
 		log.Print("ProxyShutdown:", err)
+		return 1
 	}
+	log.Print("ProxyShutdown: Success")
+	return 0
 }
 
-func ProxyFlushLogs(db *sql.DB) {
+func ProxyFlushLogs(db *sql.DB) int {
+	log.Print("ProxyFlushLogs: ", CmdProxyFlushLogs)
 	_, err := db.Query(CmdProxyFlushLogs)
 	if err != nil {
 		log.Print("ProxyFlushLogs:", err)
+		return 1
 	}
+	log.Print("ProxyFlushLogs: Success")
+	return 0
 }
 
-func ProxyKill(db *sql.DB) {
+func ProxyKill(db *sql.DB) int {
+	log.Print("ProxyKill: ", CmdProxyKill)
 	_, err := db.Query(CmdProxyKill)
 	if err != nil {
 		log.Print("ProxyKill:", err)
+		return 1
 	}
+	log.Print("ProxyKill: Success")
+	return 0
 }
 
-func LoadUserToRuntime(db *sql.DB) {
+func LoadUserToRuntime(db *sql.DB) int {
+	log.Print("LoadUserToRuntime: ", CmdLoadUserToRuntime)
 	_, err := db.Query(CmdLoadUserToRuntime)
 	if err != nil {
 		log.Print("LoadUserToRuntime:", err)
+		return 1
 	}
+	log.Print("LoadUserToRuntime: Success")
+	return 0
 }
 
-func SaveUserToDisk(db *sql.DB) {
+func SaveUserToDisk(db *sql.DB) int {
+	log.Print("SaveUserToDisk: ", CmdSaveUserToDisk)
 	_, err := db.Query(CmdSaveUserToDisk)
 	if err != nil {
 		log.Print("SaveUserToDisk", err)
+		return 1
 	}
+	log.Print("SaveUserToDisk: Success")
+	return 0
 }
 
-func LoadServerToRuntime(db *sql.DB) {
+func LoadServerToRuntime(db *sql.DB) int {
+	log.Print("LoadServerToRuntime: ", CmdLoadServerToRuntime)
 	_, err := db.Query(CmdLoadServerToRuntime)
 	if err != nil {
 		log.Print("LoadServerToRuntime:", err)
+		return 1
 	}
+	log.Print("LoadServerToRuntime: Success")
+	return 0
 }
 
-func SaveServerToDisk(db *sql.DB) {
+func SaveServerToDisk(db *sql.DB) int {
+	log.Print("SaveServerToDisk: ", CmdSaveServerToDisk)
 	_, err := db.Query(CmdSaveServerToDisk)
 	if err != nil {
 		log.Print("SaveServerToDisk:", err)
+		return 1
 	}
+	log.Print("SaveServerToDisk: Success")
+	return 0
 }
 
-func LoadQueryRulesToRuntime(db *sql.DB) {
+func LoadQueryRulesToRuntime(db *sql.DB) int {
+	log.Print("LoadQueryRulesToRuntime: ", CmdLoadQueryRulesToRuntime)
 	_, err := db.Query(CmdLoadQueryRulesToRuntime)
 	if err != nil {
 		log.Print("LoadQueryRulesToRuntime:", err)
+		return 1
 	}
+	log.Print("LoadQueryRulesToRuntime: Success")
+	return 0
 }
 
-func SaveQueryRulesToDisk(db *sql.DB) {
+func SaveQueryRulesToDisk(db *sql.DB) int {
+	log.Print("SaveQueryRulesToDisk: ", CmdSaveQueryRulesToDisk)
 	_, err := db.Query(CmdSaveQueryRulesToDisk)
 	if err != nil {
 		log.Print("SaveQueryRulesToDisk:", err)
+		return 1
 	}
+	log.Print("SaveQueryRulesToDisk: Success")
+	return 0
 }
 
-func LoadSchedulerToRuntime(db *sql.DB) {
+func LoadSchedulerToRuntime(db *sql.DB) int {
+	log.Print("LoadSchedulerToRuntime: ", CmdLoadSchedulerToRuntime)
 	_, err := db.Query(CmdLoadSchedulerToRuntime)
 	if err != nil {
 		log.Print("LoadSchedulerToRuntime:", err)
+		return 1
 	}
+	log.Print("LoadSchedulerToRuntime: Success")
+	return 0
 }
 
-func SaveSchedulerToDisk(db *sql.DB) {
+func SaveSchedulerToDisk(db *sql.DB) int {
+	log.Print("SaveSchedulerToDisk: ", CmdSaveSchedulerToDisk)
 	_, err := db.Query(CmdSaveSchedulerToDisk)
 	if err != nil {
 		log.Print("SaveSchedulerToDisk:", err)
+		return 1
 	}
+	log.Print("SaveSchedulerToDisk: Success")
+	return 0
 }

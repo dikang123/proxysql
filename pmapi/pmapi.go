07db15c8e9db3bad081c3b5e40c2d9823b928a62
Lei Tian <taylor840326@gmail.com>
2017-06-22 16:29:20 +0800

    之前管理命令api忘了写return语句

--- pmapi/pmapi.go
+++ pmapi/pmapi.go
@@ -940,79 +940,79 @@ func (pmapi *PMApi) DeleteOneQueryRules(c echo.Context) error {
 func (pmapi *PMApi) SetProxySQLReadonly(c echo.Context) error {
 	cret := cmd.ProxyReadOnly(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLReadwrite(c echo.Context) error {
 	cret := cmd.ProxyReadWrite(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLStart(c echo.Context) error {
 	cret := cmd.ProxyStart(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLRestart(c echo.Context) error {
 	cret := cmd.ProxyRestart(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLStop(c echo.Context) error {
 	cret := cmd.ProxyStop(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLPause(c echo.Context) error {
 	cret := cmd.ProxyPause(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLResume(c echo.Context) error {
 	cret := cmd.ProxyResume(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLShutdown(c echo.Context) error {
 	cret := cmd.ProxyShutdown(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLFlogs(c echo.Context) error {
 	cret := cmd.ProxyFlushLogs(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }
 
 func (pmapi *PMApi) SetProxySQLKill(c echo.Context) error {
 	cret := cmd.ProxyKill(pmapi.Apidb)
 	if cret == 0 {
-		c.JSON(http.StatusOK, "OK")
+		return c.JSON(http.StatusOK, "OK")
 	}
-	c.JSON(http.StatusExpectationFailed, "Failed")
+	return c.JSON(http.StatusExpectationFailed, "Failed")
 }

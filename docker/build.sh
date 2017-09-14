ea13f7a7ac713bccbf89488dae4cacf9c1bc59ed
Lei Tian <taylor840326@gmail.com>
2017-09-14 17:32:10 +0800

    修改docker编译环境。

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -e
+set -x
+
 export CGO_ENABLED=0 
 export GOOS=linux 
 export GOARCH=amd64
 
+GW=`docker network inspect b9d9659ce86c |grep Gateway |awk -F':' '{print $2}'|awk -F'"' '{print $2}'`
 
-go build -o proxysql-master ../main.go
+go build -o main  ../main.go
 
 docker build -t pm .
+
+rm -f main
+
+docker run -p 3333:3333 -d pm 

782939293009284c0ac57610f9d305aff7736e8e
Lei Tian <taylor840326@gmail.com>
2017-12-22 18:51:44 +0800

    modified:   queryrules.go
    [line 184~385] add some newQr functions.

--- queryrules.go
+++ queryrules.go
@@ -9,31 +9,31 @@ import (
 
 type (
 	QueryRules struct {
-		Rule_id               int64  `db:"rule_id" json:"rule_id"`
-		Active                int64  `db:"active" json:"active"`
+		Rule_id               uint64 `db:"rule_id" json:"rule_id"`
+		Active                uint64 `db:"active" json:"active"`
 		Username              string `db:"username" json:"username"`
 		Schemaname            string `db:"schemaname" json:"schemaname"`
-		FlagIN                int64  `db:"flagIN" json:"flagIN"`
+		FlagIN                uint64 `db:"flagIN" json:"flagIN"`
 		Client_addr           string `db:"client_addr" json:"client_addr"`
 		Proxy_addr            string `db:"proxy_addr" json:"proxy_addr"`
-		Proxy_port            int64  `db:"proxy_port" json:"proxy_port"`
+		Proxy_port            uint64 `db:"proxy_port" json:"proxy_port"`
 		Digest                string `db:"digest" json:"digest"`
 		Match_digest          string `db:"match_digest" json:"match_digest"`
 		Match_pattern         string `db:"match_pattern" json:"match_pattern"`
-		Negate_match_pattern  int64  `db:"negate_match_pattern" json:"negate_match_pattern"`
-		FlagOUT               int64  `db:"flagOUT" json:"flagOUT"`
+		Negate_match_pattern  uint64 `db:"negate_match_pattern" json:"negate_match_pattern"`
+		FlagOUT               uint64 `db:"flagOUT" json:"flagOUT"`
 		Replace_pattern       string `db:"replace_pattern" json:"replace_pattern"`
-		Destination_hostgroup int64  `db:"destination_hostgroup" json:"destination_hostgroup"`
-		Cache_ttl             int64  `db:"cache_ttl" json:"cache_ttl"`
-		Reconnect             int64  `db:"reconnect" json:"reconnect"`
-		Timeout               int64  `db:"timeout" json:"timeout"`
-		Retries               int64  `db:"retries" json:"retries"`
-		Delay                 int64  `db:"delay" json:"delay"`
-		Mirror_flagOUT        int64  `db:"mirror_flagOUT" json:"mirror_flagOUT"`
-		Mirror_hostgroup      int64  `db:"mirror_hostgroup" json:"mirror_hostgroup"`
+		Destination_hostgroup uint64 `db:"destination_hostgroup" json:"destination_hostgroup"`
+		Cache_ttl             uint64 `db:"cache_ttl" json:"cache_ttl"`
+		Reconnect             uint64 `db:"reconnect" json:"reconnect"`
+		Timeout               uint64 `db:"timeout" json:"timeout"`
+		Retries               uint64 `db:"retries" json:"retries"`
+		Delay                 uint64 `db:"delay" json:"delay"`
+		Mirror_flagOUT        uint64 `db:"mirror_flagOUT" json:"mirror_flagOUT"`
+		Mirror_hostgroup      uint64 `db:"mirror_hostgroup" json:"mirror_hostgroup"`
 		Error_msg             string `db:"error_msg" json:"error_msg"`
-		Log                   int64  `db:"log" json:"log"`
-		Apply                 int64  `db:"apply" json:"apply"`
+		Log                   uint64 `db:"log" json:"log"`
+		Apply                 uint64 `db:"apply" json:"apply"`
 		Comment               string `db:"comment" json:"comment"`
 	}
 )
@@ -179,6 +179,208 @@ func (qr *QueryRules) FindAllQr(db *sql.DB, limit int64, skip int64) ([]QueryRul
 		AllQr = append(AllQr, tmpqr)
 	}
 	return AllQr, nil
+}
+
+// new mysql query rules
+func NewQr(username string, destination_hostgroup uint64) (*QueryRules, error) {
+	newqr := new(QueryRules)
+
+	newqr.Username = username
+	newqr.Destination_hostgroup = destination_hostgroup
+
+	newqr.Schemaname = "NULL"
+	newqr.FlagIN = 0
+	newqr.Client_addr = "NULL"
+	newqr.Proxy_addr = "NULL"
+	newqr.Proxy_port = 0
+	newqr.Digest = "NULL"
+	newqr.Match_digest = "NULL"
+	newqr.Match_pattern = "NULL"
+	newqr.Negate_match_pattern = 0
+	newqr.FlagOUT = 0
+	newqr.Replace_pattern = "NULL"
+	newqr.Cache_ttl = 0
+	newqr.Reconnect = 0
+	newqr.Timeout = 0
+	newqr.Retries = 0
+	newqr.Delay = 0
+	newqr.Mirror_flagOUT = 0
+	newqr.Mirror_hostgroup = 0
+	newqr.Error_msg = "NULL"
+	newqr.Log = 0
+	newqr.Apply = 0
+	newqr.Active = 0
+	newqr.Comment = "NULL"
+
+	return newqr, nil
+}
+
+// set qr active
+func (qr *QueryRules) SetQrActive(active uint64) {
+	switch active {
+	case 0:
+		qr.Active = 0
+	case 1:
+		qr.Active = 1
+	default:
+		qr.Active = 1
+	}
+}
+
+// set qr apply
+func (qr *QueryRules) SetQrApply(apply uint64) {
+	switch apply {
+	case 0:
+		qr.Apply = 0
+	case 1:
+		qr.Apply = 1
+	default:
+		qr.Apply = 1
+	}
+}
+
+// set qr schemaname
+func (qr *QueryRules) SetQrSchemaname(schema_name string) {
+	if schema_name == "" || len(schema_name) == 0 {
+		qr.Schemaname = "NULL"
+	} else {
+		qr.Schemaname = schema_name
+	}
+}
+
+// set qr flagIN
+func (qr *QueryRules) SetQrFlagIN(flag_in uint64) {
+	qr.FlagIN = flag_in
+}
+
+// set qr client_addr
+func (qr *QueryRules) SetQrClientAddr(client_addr string) {
+	if client_addr == "" || len(client_addr) == 0 {
+		qr.Client_addr = "NULL"
+	} else {
+		qr.Client_addr = client_addr
+	}
+}
+
+// set qr proxy_addr
+func (qr *QueryRules) SetQrProxyAddr(proxy_addr string) {
+	if proxy_addr == "" || len(proxy_addr) == 0 {
+		qr.Proxy_addr = "NULL"
+	} else {
+		qr.Proxy_addr = proxy_addr
+	}
+}
+
+// set qr proxy_port
+func (qr *QueryRules) SetProxyPort(proxy_port uint64) {
+	qr.Proxy_port = proxy_port
+}
+
+// set qr digest
+func (qr *QueryRules) SetQrDigest(digest string) {
+	if digest == "" || len(digest) == 0 {
+		qr.Digest = "NULL"
+	} else {
+		qr.Digest = digest
+	}
+}
+
+// set qr match_digest
+func (qr *QueryRules) SetQrMatchDigest(match_digest string) {
+	if match_digest == "" || len(match_digest) == 0 {
+		qr.Match_digest = "NULL"
+	} else {
+		qr.Match_digest = match_digest
+	}
+}
+
+// set qr match_pattern
+func (qr *QueryRules) SetQrMatchPattern(match_pattern string) {
+	if match_pattern == "" || len(match_pattern) == 0 {
+		qr.Match_pattern = "NULL"
+	} else {
+		qr.Match_pattern = match_pattern
+	}
+}
+
+// set qr mnegate_match_pattern
+func (qr *QueryRules) SetQrNegateMatchPattern(negate_match_pattern uint64) {
+	switch negate_match_pattern {
+	case 0:
+		qr.Negate_match_pattern = 0
+	case 1:
+		qr.Negate_match_pattern = 1
+	default:
+		qr.Negate_match_pattern = 0
+	}
+}
+
+// set qr flagout
+func (qr *QueryRules) SetQrFlagOut(flag_out uint64) {
+	qr.FlagOUT = flag_out
+}
+
+// set qr replace_pattern
+func (qr *QueryRules) SetQrReplacePattern(replace_pattern string) {
+	if replace_pattern == "" || len(replace_pattern) == 0 {
+		qr.Replace_pattern = "NULL"
+	} else {
+		qr.Replace_pattern = replace_pattern
+	}
+}
+
+// set qr destination_hostgroup
+func (qr *QueryRules) SetQrDestHostGroup(destination_hostgroup uint64) {
+	qr.Destination_hostgroup = destination_hostgroup
+}
+
+// set qr cache_ttl
+func (qr *QueryRules) SetQrCacheTTL(cache_ttl uint64) {
+	qr.Cache_ttl = cache_ttl
+}
+
+// set qr reconnect
+func (qr *QueryRules) SetQrReconnect(reconnect uint64) {
+	qr.Reconnect = reconnect
+}
+
+// set qr timeout
+func (qr *QueryRules) SetQrTimeOut(timeout uint64) {
+	qr.Timeout = timeout
+}
+
+// set qr retries
+func (qr *QueryRules) SetQrRetries(retries uint64) {
+	qr.Retries = retries
+}
+
+// set qr delay
+func (qr *QueryRules) SetQrDelay(delay uint64) {
+	qr.Delay = delay
+}
+
+// set qr mirror_flagout
+func (qr *QueryRules) SetQrMirrorFlagOUT(mirror_flagout uint64) {
+	qr.Mirror_flagOUT = mirror_flagout
+}
+
+// set qr mirror_hostgroup
+func (qr *QueryRules) SetQrMirrorHostgroup(mirror_hostgroup uint64) {
+	qr.Mirror_hostgroup = mirror_hostgroup
+}
+
+// set qr error_msg
+func (qr *QueryRules) SetQrErrorMsg(error_msg string) {
+	if error_msg == "" || len(error_msg) == 0 {
+		qr.Error_msg = "NULL"
+	} else {
+		qr.Error_msg = error_msg
+	}
+}
+
+// set qr log
+func (qr *QueryRules) SetQrLog(log uint64) {
+	qr.Log = log
 }
 
 // add a new query rules.

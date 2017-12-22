6021b53b5165221fd0fec371476eb7e4d2cf7211
Lei Tian <taylor840326@gmail.com>
2017-12-22 16:55:24 +0800

    add users_test.go.

---
+++ users_test.go
@@ -0,0 +1,47 @@
+package proxysql
+
+import (
+	"testing"
+)
+
+func TestFindAllUsers(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	allusers, err := FindAllUserInfo(db, 1, 0)
+	if err != nil {
+		t.Error(allusers, err)
+	}
+
+}
+
+func TestAddOneUser(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	newuser := new(Users)
+	newuser.SetDefaultHostgroup(0)
+	newuser.SetDefaultSchema("dev")
+	newuser.SetUserName("devtest")
+	newuser.SetUserPass("devtest")
+
+	err = newuser.AddOneUser(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+}

b200176b68bdd21baa71a9c774c722a98168fc11
Lei Tian <taylor840326@gmail.com>
2017-12-22 15:21:59 +0800

    update users.go.

--- users.go
+++ users.go
@@ -1,20 +1,10 @@
 package proxysql
 
-/*
-* 1.获取当前用户列表
-* 2.通过用户名获取用户的详细信息
-* 3.添加一个新用户
-* 4.删除一个用户
-* 5.激活一个用户
-* 6.反激活一个用户
-* 7.更新用户信息
-*
- */
-
 import (
 	"database/sql"
 	"fmt"
-	"log"
+
+	"github.com/juju/errors"
 )
 
 type (
@@ -35,13 +25,13 @@ type (
 )
 
 const (
-	/*新建一个用户*/
+	/*add a new users*/
 	StmtAddOneUser = `
 	INSERT INTO 
 		mysql_users(username,password,default_schema) 
 	VALUES(%q,%q,%q)`
 
-	/*删除一个用户*/
+	/*delete a user*/
 	StmtDeleteOneUser = `
 	DELETE FROM 
 		mysql_users 
@@ -53,7 +43,7 @@ const (
 		frontend = %d
 	`
 
-	/*查询出所有用户的信息*/
+	/*list all users*/
 	StmtFindAllUserInfo = `
 	SELECT 
 		ifnull(username,""),
@@ -72,7 +62,7 @@ const (
 	LIMIT %d 
 	OFFSET %d`
 
-	/*更新一个用户信息*/
+	/*update a users*/
 	StmtUpdateOneUser = `
 	UPDATE 
 		mysql_users 
@@ -98,23 +88,18 @@ const (
 )
 
 func (users *Users) FindAllUserInfo(db *sql.DB, limit int64, skip int64) ([]Users, error) {
-	/*定义一个新的变量，alluser用户保存所有用户信息*/
 	var alluser []Users
 
 	Query := fmt.Sprintf(StmtFindAllUserInfo, limit, skip)
-	log.Printf("admin->users.go->FindAllUserInfo->Query :", Query)
 
 	rows, err := db.Query(Query)
 	if err != nil {
-		log.Print("admin->users.go->FindAllUserInfo=->db.Query Failed:", err)
-		return []Users{}, err
+		return []Users{}, errors.Trace(err)
 	}
 	defer rows.Close()
 
-	/*得出查询结果*/
 	for rows.Next() {
 
-		/*定义一个临时用户信息*/
 		var tmpusr Users
 
 		err = rows.Scan(
@@ -133,58 +118,48 @@ func (users *Users) FindAllUserInfo(db *sql.DB, limit int64, skip int64) ([]User
 		)
 
 		if err != nil {
-			log.Print("admin->users.go->FindAllUserInfo->rows.Scan Failed:", err)
 			continue
 		}
-
-		log.Print("admin->users.go->FindAllUserInfo->rows.Scan->tmpusr ", tmpusr)
 
 		alluser = append(alluser, tmpusr)
 	}
 	return alluser, nil
 }
 
-func (users *Users) AddOneUser(db *sql.DB) (int, error) {
+//add a new user.
+func (users *Users) AddOneUser(db *sql.DB) error {
 
 	Query := fmt.Sprintf(StmtAddOneUser, users.Username, users.Password, users.DefaultSchema)
-	log.Print("admin->users.go->AddOneUser->Query :", Query)
 
-	res, err := db.Exec(Query)
+	_, err := db.Exec(Query)
 	if err != nil {
-		log.Print("admin->users.go->AddOneUser->db.Exec Failed:", err)
-		return 1, err //add user failed
+		return errors.Trace(err) //add user failed
 	}
-
-	rowsAffected, err := res.RowsAffected()
-	log.Print("admin->users.go->AddOneUser->RowsAffected: ", rowsAffected)
 
 	LoadUserToRuntime(db)
 	SaveUserToDisk(db)
-	return 0, nil
+
+	return nil
 }
 
-func (users *Users) DeleteOneUser(db *sql.DB) (int, error) {
+//delete a user.
+func (users *Users) DeleteOneUser(db *sql.DB) error {
 
 	Query := fmt.Sprintf(StmtDeleteOneUser, users.Username, users.Backend, users.Frontend)
-	log.Print("admin->users.go->DeleteOneUser->Query:", Query)
 
-	res, err := db.Exec(Query)
+	_, err := db.Exec(Query)
 	if err != nil {
-		log.Print("admin->users.go->DeleteOneUser->db.Exec Failed:", err)
-		return 1, err //delte failed
+		return errors.Trace(err) //delte failed
 	}
-
-	rowsAffected, err := res.RowsAffected()
-	log.Print("admin->users.go->DeleteOneUser->RowsAffected:", rowsAffected)
 
 	LoadUserToRuntime(db)
 	SaveUserToDisk(db)
-	return 0, nil //delete success
 
+	return nil //delete success
 }
 
-// 更新一个用户所有信息，使用PATCH方法
-func (users *Users) UpdateOneUserInfo(db *sql.DB) (int, error) {
+// update a user.
+func (users *Users) UpdateOneUserInfo(db *sql.DB) error {
 
 	Query := fmt.Sprintf(StmtUpdateOneUser,
 		users.Password,
@@ -202,19 +177,13 @@ func (users *Users) UpdateOneUserInfo(db *sql.DB) (int, error) {
 		users.Backend,
 		users.Frontend)
 
-	log.Print("users->UpdateOneUserInfo->st: ", Query)
-
-	res, err := db.Exec(Query)
+	_, err := db.Exec(Query)
 	if err != nil {
-		log.Print("admin->users.go->UpdateOneUserInfo->db.Exec Failed :", err)
-		return 1, err
+		return errors.Trace(err)
 	}
-
-	rowsAffected, err := res.RowsAffected()
-	log.Print("admin->users.go->UpdateOneUser->RowsAffected: ", rowsAffected)
 
 	LoadUserToRuntime(db)
 	SaveUserToDisk(db)
 
-	return 0, nil
+	return nil
 }

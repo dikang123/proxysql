0381b975f0317d9d14d65f51ce30ff029e4585fa
Lei Tian <taylor840326@gmail.com>
2017-12-22 19:14:15 +0800

    add queryrules_test.go

---
+++ queryrules_test.go
@@ -0,0 +1,97 @@
+package proxysql
+
+import (
+	"testing"
+)
+
+func TestFindAllQr(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	allqr, err := FindAllQr(db, 1, 0)
+	if err != nil {
+		t.Error(allqr, err)
+	}
+
+}
+func TestAddOneQr(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	newqr, err := NewQr("dev", 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = newqr.AddOneQr(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestDeleteOneQr(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	newqr, err := NewQr("dev", 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	newqr.SetQrRuleid(2)
+
+	err = newqr.DeleteOneQr(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func TestUpdateOneQr(t *testing.T) {
+	conn, err := NewConn("172.18.10.111", 13306, "admin", "admin")
+	if err != nil {
+		t.Error(conn, err)
+	}
+
+	db, err := conn.OpenConn()
+	if err != nil {
+		t.Error(db, err)
+	}
+
+	newqr, err := NewQr("dev", 0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	newqr.SetQrRuleid(4)
+	newqr.SetQrActive(1)
+	newqr.SetQrMatchDigest("^SELECT")
+
+	err = newqr.UpdateOneQrInfo(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
